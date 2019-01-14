dff44ea010d467cb36a40aff586633a6ea334e51
fangmingming <42854837+fangmingming@users.noreply.github.com>
2019-01-14 15:57:32 +0800

    Add files via upload

---
+++ good_list.php
@@ -0,0 +1,37 @@
+<?php
+
+$goodList = [
+	'0' => [
+		'id' => '1',
+		'name' => '商品一',
+		'price' => '10',
+		'des' => '商品一的介绍',
+		'imgUrl' => 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1547451902&di=c04731885e83e9b54d839941806be8e1&src=http://d.hiphotos.baidu.com/lvpics/w=600/sign=4752e2c8a5c27d1ea52638c42bd4adaf/79f0f736afc379319938d645eac4b74543a9110d.jpg'
+	],
+	'1' => [
+		'id' => '2',
+		'name' => '商品二',
+		'price' => '20',
+		'des' => '商品二的介绍',
+		'imgUrl' => 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547461987850&di=f498496c601755b1d6a7697ce2f41751&imgtype=0&src=http%3A%2F%2Fimg006.hc360.cn%2Fg6%2FM00%2F9D%2F8E%2FwKhQsFOw3QaEbm2hAAAAADxx9rs362.jpg'
+	],
+	'2' => [
+		'id' => '3',
+		'name' => '商品三',
+		'price' => '30',
+		'des' => '商品三的介绍',
+		'imgUrl' => 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547461987850&di=66a8d16b31445192b0e05ff1a4e09c73&imgtype=0&src=http%3A%2F%2Fimg008.hc360.cn%2Fy3%2FM07%2FD1%2F73%2FwKhQh1UxSfiEdOBXAAAAAHL5HhQ519.jpg'
+        ],
+	'3' => [
+		'id' => '4',
+		'name' => '商品四',
+		'price' => '40',
+		'des' => '商品四的介绍',
+		'imgUrl' => 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547461987849&di=a52ecf38af467d851da40a92edd65b7b&imgtype=0&src=http%3A%2F%2Fimg.yougou.com%2Fpics%2Fadidas%2F2012%2F21006606%2F21006606_03_mb.jpg'
+	]
+
+
+];
+return json_encode($goodList);
+
+?>
